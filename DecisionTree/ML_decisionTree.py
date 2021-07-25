cce4d55550465ee50cc657b12ac4d7d308019e24
AoDaDou <ecupl_zhangwei@163.com>
2021-07-25 23:35:50 +0800

    Add C4.5 Decision Tree

--- DecisionTree/ML_decisionTree.py
+++ DecisionTree/ML_decisionTree.py
@@ -7,7 +7,7 @@ Created on Tue Jun 29 22:40:19 2021
 
 
 import numpy as np
-from utils import calculate_entropy, calculate_yproba
+from utils import *
 
 
 # from sklearn.tree import DecisionTreeClassifier
@@ -61,7 +61,7 @@ class BaseTree(object):
         self._search_feature_method = None             #结点特征选择方法
         self._split_dataset_method = None              #结点样本拆分方法
     
-    def _build_tree(self, X, y, node_depth=0, column_name=None):
+    def _build_tree(self, X, y, node_depth=0, column_indicator=None):
         """
         树的构造方法。
 
@@ -73,8 +73,8 @@ class BaseTree(object):
             该结点上的标签，分类结果或者数值.
         node_depth : Int
             该结点的树深.
-        column_name : List
-            该结点上样本的列名称.
+        column_indicator : List
+            该结点上样本的列的指针.
 
         Returns
         -------
@@ -82,7 +82,6 @@ class BaseTree(object):
             该结点的信息，无法再分返回None.
 
         """
-        
         node_value = self._calculate_nodevalue_method(y)
         n_samples, n_features = X.shape        
         # 划分树的话，另child_node为None，条件如下：
@@ -97,14 +96,15 @@ class BaseTree(object):
         # 不停止划分的话，另child_node为字典：
         child_node = {}
         ## 1.找到树的最优特征和划分方法
-        best_feature, thres_list = self._search_feature_method(X, y, column_name)
+        best_feature, thres_list = self._search_feature_method(X, y, column_indicator)
         ## 2.遍历保存树的子结点
         for thres in thres_list:
-            newx, newy, newcolumnname = self._split_dataset_method(X, y, best_feature, thres, column_name)
+            newx, newy, newcolindicator = self._split_dataset_method(X, y, best_feature, thres, column_indicator)
             new_depth = node_depth + 1
-            child_node[thres] = self._build_tree(newx, newy, new_depth, newcolumnname)
+            child_node[thres] = self._build_tree(newx, newy, new_depth, newcolindicator)
         node = TreeNode(best_feature, thres_list, node_value, node_depth, n_samples, child_node)
         return node
+
 
 
 # 分类树
@@ -128,55 +128,67 @@ class DecisionTreeClassifier(BaseTree):
         super(DecisionTreeClassifier, self).__init__(criterion, minloss)
     
     
-    def __search_feature_entropy(self, X, y, column_name=None):
-        # 根据特征值、标签值、根信息熵计算信息增益
-        def calculate_gain_entropy(xarray, yarray, root_entropy):
-            xSet = np.unique(xarray)
-            Di = [sum(xarray==value)/len(xarray) for value in xSet]
-            Child_entropy = [calculate_entropy(y[np.nonzero(xarray==value)[0]]) for value in xSet]
-            gain_entropy = root_entropy - np.dot(Di, Child_entropy)            
-            return gain_entropy, list(xSet)
+    def __search_feature_entropy(self, X, y, column_indicator=None):
         # 遍历特征，找到最大信息增益的特征
         n_samples, n_features = X.shape
-        best_gain_entropy = 0
+        best_entropy_gainratio = 0
         best_feature_index = None
-        best_thres_list = None
-        entropy0 = calculate_entropy(y)
+        best_feature_value = None
         for idx in range(n_features):
-            gain_entropy, thres_list = calculate_gain_entropy(X[:,idx], y, entropy0)
-            if gain_entropy > best_gain_entropy:
-                best_gain_entropy = gain_entropy
+            if self.is_categorical.__len__() > 0 and self.is_categorical[column_indicator[idx]]==False:
+                # 连续特征求信息增益率
+                entropy_gainratio, feature_value = ent_gainratio_continuous(X[:, idx], y)
+            else:
+                # 分类特征求信息增益率
+                entropy_gainratio, feature_value = ent_gainratio_categorical(X[:, idx], y)
+            if entropy_gainratio > best_entropy_gainratio:
+                best_entropy_gainratio = entropy_gainratio
                 best_feature_index = idx
-                best_thres_list = thres_list
-        return column_name[best_feature_index], best_thres_list
+                best_feature_value = feature_value
+        return column_indicator[best_feature_index], best_feature_value
     
     
-    def __split_dataset_entropy(self, X, y, best_feature, feature_thres, column_name):
-        best_feature_idx = column_name.index(best_feature)
-        newx, newy, newcolumnname = np.copy(X), np.copy(y), list(np.copy(column_name))
-        newx = np.delete(newx[np.nonzero(X[:,best_feature_idx]==feature_thres)[0],:], best_feature_idx, axis=1)
-        newy = newy[np.nonzero(X[:,best_feature_idx]==feature_thres)[0]]
-        newcolumnname.remove(best_feature)
-        return newx, newy, newcolumnname
+    def __split_dataset_entropy(self, X, y, best_feature, feature_thres, column_indicator):
+        best_feature_idx = column_indicator.index(best_feature)
+        newx, newy, newcolindicator = np.copy(X), np.copy(y), list(np.copy(column_indicator))
+        if self.is_categorical.__len__() > 0 and self.is_categorical[best_feature]==False:
+            # 连续特征拆解数据集
+            if feature_thres[1] == 'maximum':
+                newx = newx[np.nonzero(X[:, best_feature_idx] <= feature_thres[0])[0], :]
+                newy = newy[np.nonzero(X[:, best_feature_idx] <= feature_thres[0])[0]]
+            elif feature_thres[1] == 'minimum':
+                newx = newx[np.nonzero(X[:, best_feature_idx] > feature_thres[0])[0], :]
+                newy = newy[np.nonzero(X[:, best_feature_idx] > feature_thres[0])[0]]
+            else:
+                pass
+        else:
+            # 分类特征拆解数据集
+            newx = np.delete(newx[np.nonzero(X[:,best_feature_idx]==feature_thres)[0],:], best_feature_idx, axis=1)
+            newy = newy[np.nonzero(X[:,best_feature_idx]==feature_thres)[0]]
+            newcolindicator.remove(best_feature)
+        return newx, newy, newcolindicator
 
     
-    def fit(self, X, y):
+    def fit(self, X, y, is_categorical:list=[]):
         # 赋值训练要用到的方法
         self._calculate_loss = calculate_entropy
         self._calculate_nodevalue_method = calculate_yproba
         self._search_feature_method = self.__search_feature_entropy
         self._split_dataset_method = self.__split_dataset_entropy
-        # 构建树
-        self.tree = self._build_tree(X, y, column_name=list(range(X.shape[1])))
+        # 其他参数
+        self.is_categorical = is_categorical
+        # 构建树的参数
+        self.tree = self._build_tree(X, y, column_indicator=list(range(X.shape[1])))
         
 
 
 def print_tree(tree):
+    columns = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感', '密度', '含糖率']
     print(f"========第{tree.depth}层========")
     print(f"该树的结点类型为{tree.value}")
     if tree.childNode is None:
         return
-    print(f"最优划分特征是{tree.feature_i}，划分值有{tree.feature_thres}")
+    print(f"最优划分特征是{columns[tree.feature_i]}，划分值有{tree.feature_thres}")
     for i, node in tree.childNode.items():
         print(f"\n当子结点的属性值为{i}时：")
         print_tree(node)
@@ -192,5 +204,12 @@ if __name__ == "__main__":
     tree_id3_classifier.tree
     print_tree(tree_id3_classifier.tree)
     
-    
+    # C4.5对西瓜集数据分类
+    tree_c45_classifier = DecisionTreeClassifier(criterion="entropy", minloss=0.0001)
+    tree_c45_classifier.fit(Xdata, Ylabel, [True, True, True, True, True, True, False, False])
+    tree_c45_classifier.tree
+    print_tree(tree_c45_classifier.tree)
+
+
+
     
--- DecisionTree/utils.py
+++ DecisionTree/utils.py
@@ -27,4 +27,41 @@ def calculate_yproba(y):
     return ydict
     
 
-# 拆分数据集函数
+# 计算信息增益率——分类变量
+def ent_gainratio_categorical(xarray, yarray):
+    root_entropy = calculate_entropy(yarray)
+    xSet = np.unique(xarray)
+    Di = [sum(xarray == value) / len(xarray) for value in xSet]
+    Child_entropy = [calculate_entropy(yarray[np.nonzero(xarray == value)[0]]) for value in xSet]
+    # ID3，计算信息增益
+    gain_entropy = root_entropy - np.dot(Di, Child_entropy)
+    # C4.5，计算信息增益率
+    iv = calculate_entropy(xarray)
+    gain_entropy_ratio = gain_entropy / iv
+    return gain_entropy_ratio, list(xSet)
+
+
+# 计算信息增益率——连续变量
+def ent_gainratio_continuous(xarray, yarray):
+    root_entropy = calculate_entropy(yarray)
+    xSet = np.unique(xarray)
+    # best_gain_entropy = 0
+    best_gain_entropy_ratio = 0
+    best_feature_value = None
+    feature_value_list = [(xSet[i] + xSet[i + 1]) / 2 for i in range(len(xSet) - 1)]
+    for value in feature_value_list:
+        Di = [sum(xarray <= value) / len(xarray) , sum(xarray > value) / len(xarray)]
+        Child_entropy = [calculate_entropy(yarray[np.nonzero(xarray < value)[0]]), calculate_entropy(yarray[np.nonzero(xarray > value)[0]])]
+        # ID3，计算信息增益
+        gain_entropy = root_entropy - np.dot(Di, Child_entropy)
+        # C4.5，计算信息增益率
+        iv = calculate_entropy(xarray)
+        gain_entropy_ratio = gain_entropy / iv
+        if gain_entropy_ratio > best_gain_entropy_ratio:
+            # best_gain_entropy = gain_entropy
+            best_gain_entropy_ratio = gain_entropy_ratio
+            best_feature_value = value
+    return best_gain_entropy_ratio, [(best_feature_value, 'maximum'), (best_feature_value, 'minimum')]
+
+
+
